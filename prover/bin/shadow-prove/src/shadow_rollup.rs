f052244d3f9b2c1cd706e9a8858fbedc0b606f9d
jasmylon <3770310446@qq.com>
2025-08-25 17:00:08 +0800

    Update shadow_rollup.rs

--- prover/bin/shadow-prove/src/shadow_rollup.rs
+++ prover/bin/shadow-prove/src/shadow_rollup.rs
@@ -75,7 +75,7 @@ where
             return Ok(None);
         };
 
-        // Assembling a batche of the same commitment.
+        // Assembling a batch of the same commitment.
         #[rustfmt::skip]
         //   Below is the encoding for `BatchHeader`, reference: morph-repo/contracts/contracts/libraries/codec/BatchHeaderCodecV1.sol
         //    
@@ -92,7 +92,7 @@ where
         //   * sequencerSetVerifyHash  32          bytes32     185     L2 sequencers set verify hash
         //   * parentBatchHash         32          bytes32     217     The parent batch hash
         //   * skippedL1MessageBitmap  dynamic     uint256[]   249     A bitmap to indicate which L1 messages are skipped in the batch
-        //   @dev Below is the feilds for `BatchHeader` V1
+        //   @dev Below is the fields for `BatchHeader` V1
         //   * lastBlockNumber         8           uint64      249     The last block number in this batch
         // ```
         let batch_store = ShadowRollup::BatchStore {
